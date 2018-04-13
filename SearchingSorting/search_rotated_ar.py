7d87b9efa6f998da81b04af4508e50e33b9d64c8
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-04-13 14:11:18 +0900

    search rotated ar

---
+++ SearchingSorting/search_rotated_ar.py
@@ -0,0 +1,21 @@
+
+def search_rotated_ar(nums, target):
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > nums[end]:
+            if nums[start] <= target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if nums[mid] < target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+    return -1
+
+print (search_rotated_ar([4, 5, 6, 7, 0, 1, 2], 5))
