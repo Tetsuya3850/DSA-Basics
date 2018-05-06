3eba0ea47668ed832ed90e2b4e26c604a0686fed
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-05-06 10:22:19 +0900

    majority element

---
+++ ArraysStrings/majority_element.py
@@ -0,0 +1,30 @@
+# Given a positive integers array, find the majority element.
+
+
+def majority_element(A):
+    candidate_majority_element = find_candidate_majority_element(A)
+    return candidate_majority_element if validate_candidate_element(A, candidate_majority_element) else -1
+
+
+def find_candidate_majority_element(A):
+    majority = 0
+    count = 0
+    for integer in A:
+        if count == 0:
+            majority = integer
+        if integer == majority:
+            count += 1
+        else:
+            count -= 1
+    return majority
+
+
+def validate_candidate_element(A, majority):
+    count = 0
+    for integer in A:
+        if integer == majority:
+            count += 1
+    return count > len(A) / 2
+
+
+print(majority_element([5, 5, 5, 9, 9, 9, 1, 5, 5]))
