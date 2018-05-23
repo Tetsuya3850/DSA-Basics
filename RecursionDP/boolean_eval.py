1705e49137daca38c08c3499116e97a40d4bed1e
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-05-23 12:05:38 +0900

    boolean eval and recursive multiply

---
+++ RecursionDP/boolean_eval.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+
+def count_eval(s, result):
+    def helper(s, result, cache):
+        if len(s) == 0:
+            return 0
+
+        if len(s) == 1:
+            return 1 if (bool(int(s)) == result) else 0
+
+        if (s, result) not in cache:
+            ways = 0
+            for i in range(1, len(s), 2):
+                c = s[i]
+                left = s[:i]
+                right = s[i+1:]
+                leftTrue = helper(left, True, cache)
+                leftFalse = helper(left, False, cache)
+                rightTrue = helper(right, True, cache)
+                rightFalse = helper(right, False, cache)
+                total = (leftTrue + leftFalse) * (rightTrue + rightFalse)
+
+                totalTrue = 0
+                if c == '^':
+                    totalTrue = leftTrue * rightFalse + leftFalse * rightTrue
+                elif c == '&':
+                    totalTrue = leftTrue * rightTrue
+                elif c == '|':
+                    totalTrue = leftTrue * rightTrue + leftTrue * rightFalse + leftFalse * rightTrue
+
+                subWays = totalTrue if result else total - totalTrue
+                ways += subWays
+            cache[(s, result)] = ways
+
+        return cache[(s, result)]
+
+    cache = defaultdict()
+    return helper(s, result, cache)
+
+
+print(count_eval('1^0|0|1', False))
+print(count_eval('0&0&0&1^1|0', True))
---
+++ RecursionDP/recursive_multiply.py
@@ -0,0 +1,20 @@
+def multiply_recursive(a, b):
+    # Time O(logk), Space O(logk), where k is the smaller of the two.
+    def multiply_helper(smaller, bigger):
+        if smaller == 0:
+            return 0
+        elif smaller == 1:
+            return bigger
+        half = smaller >> 1
+        half_prod = multiply_helper(half, bigger)
+        if a % 2 == 0:
+            return half_prod + half_prod
+        else:
+            return half_prod + half_prod + bigger
+
+    bigger = max(a, b)
+    smaller = min(a, b)
+    return multiply_helper(smaller, bigger)
+
+
+print(multiply_recursive(8, 9))
