53e825a410bd421a59eabeef553b1f13c4371d28
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-05-23 14:30:25 +0900

    rotated search updated

--- SearchingSorting/search_rotated_ar.py
+++ SearchingSorting/search_rotated_ar.py
@@ -1,22 +1,34 @@
 def search_rotated_ar(nums, target):
     # Time O(logN), Space O(1), where N is the length of the array.
-    start = 0
-    end = len(nums) - 1
-    while start <= end:
-        mid = (start + end) // 2
+    def helper(nums, target, left, right):
+        mid = (left + right) // 2
         if nums[mid] == target:
             return mid
-        elif nums[mid] > nums[end]:
-            if nums[start] <= target < nums[mid]:
-                end = mid - 1
+        if right < left:
+            return -1
+
+        if nums[left] < nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                return helper(nums, target, left, mid-1)
             else:
-                start = mid + 1
-        else:
-            if nums[mid] < target <= nums[end]:
-                start = mid + 1
+                return helper(nums, target, mid+1, right)
+        elif nums[left] > nums[mid]:
+            if nums[mid] < target <= nums[right]:
+                return helper(nums, target, mid+1, right)
             else:
-                end = mid - 1
-    return -1
+                return helper(nums, target, left, mid-1)
+        elif nums[left] == nums[mid]:
+            if nums[mid] != nums[right]:
+                return helper(nums, target, mid+1, right)
+            else:
+                result = helper(nums, target, left, mid-1)
+                if result == -1:
+                    return helper(nums, target, mid+1, right)
+                else:
+                    return result
+        return -1
+
+    return helper(nums, target, 0, len(nums)-1)
 
 
 print(search_rotated_ar([4, 5, 6, 7, 0, 1, 2], 5))
