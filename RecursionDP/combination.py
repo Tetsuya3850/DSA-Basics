61e6dc3caffcc4e4d7bff3d2f6fb742b3ca39307
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-05-26 18:53:02 +0900

    combination

--- RecursionDP/combination.py
+++ RecursionDP/combination.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 
 
-def combination(self, n, k):
+def combination(n, k):
     def helper(n, k):
-        if k == 1 or n == k:
+        if k == 0:
+            return [[]]
+        if k == 1:
             return [[(i)] for i in range(1, n+1)]
+        elif k == n:
+            return [[i for i in range(1, n+1)]]
         if (n, k) not in cache:
             results = []
             without_n = helper(n-1, k)
@@ -16,3 +20,23 @@ def combination(self, n, k):
         return cache[(n, k)]
     cache = defaultdict()
     return helper(n, k)
+
+
+print(combination(5, 4))
+
+
+def combination_num(n, k):
+    # Time O(nk), Space O(nk).
+    def helper(n, k):
+        if k == 0 or k == n:
+            return 1
+        if (n, k) not in cache:
+            without_n = helper(n-1, k)
+            with_n = helper(n-1, k-1)
+            cache[(n, k)] = without_n + with_n
+        return cache[(n, k)]
+    cache = defaultdict()
+    return helper(n, k)
+
+
+print(combination_num(5, 3))
