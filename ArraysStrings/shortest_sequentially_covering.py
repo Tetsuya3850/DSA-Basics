a7a89d9f4e669c37cf22ef421a4d6dbbc8e1e49e
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-06-03 19:51:34 +0900

    line intersection and shortest sequentially covering

---
+++ ArraysStrings/shortest_sequentially_covering.py
@@ -0,0 +1,24 @@
+
+def smallest_sequentially_covering(paragraph, keywords):
+    # Time O(N), Space O(M), where N is the length of the paragraph and M is the length of keywords
+    keyword_to_idx = {k: i for i, k in enumerate(keywords)}
+    latest_occurrence = [-1] * len(keywords)
+    shortest_subarray_length = [float("inf")] * len(keywords)
+    shortest_dist = float('inf')
+    result = (-1, -1)
+    for i, p in enumerate(paragraph):
+        if p in keyword_to_idx:
+            keyword_idx = keyword_to_idx[p]
+            if keyword_idx == 0:
+                shortest_subarray_length[keyword_idx] = 1
+            elif shortest_subarray_length[keyword_idx-1] != float('inf'):
+                distance_to_previous_keyword = (
+                    i - latest_occurrence[keyword_idx - 1])
+                shortest_subarray_length[keyword_idx] = distance_to_previous_keyword + \
+                    shortest_subarray_length[keyword_idx-1]
+            latest_occurrence[keyword_idx] = i
+
+            if keyword_idx == len(keywords) - 1 and shortest_subarray_length[-1] < shortest_dist:
+                shortest_dist = shortest_subarray_length[-1]
+                result = (i-shortest_dist+1, i)
+    return result
---
+++ BitMath/line_intersection.py
@@ -0,0 +1,42 @@
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, start, end):
+        deltaY = end.y - start.y
+        deltaX = end.x - start.x
+        self.slope = deltaY / deltaX
+        self.yintercept = end.y - self.slope * end.x
+
+
+def is_between(start, middle, end):
+    return start.x <= middle.x <= end.x and start.y <= middle.y <= end.y
+
+
+def intersection(start1, end1, start2, end2):
+    if start1.x > end1.x:
+        start1, end1 = end1, start1
+    if start2.x > end2.x:
+        start2, end2 = end2, start2
+    if start1.x > start2.x:
+        start1, start2 = start2, start1
+        end1, end2 = end2, end1
+
+    line1 = Line(start1, end1)
+    line2 = Line(start2, end2)
+
+    if line1.slope == line2.slope:
+        if line1.yintercept == line2.yintercept and is_between(start1, start2, end1):
+            return start2
+        return None
+
+    x = (line2.yintercept - line1.yintercept) / (line1.slope - line2.slope)
+    y = x * line1.slope + line1.yintercept
+    intersection = Point(x, y)
+    if is_between(start1, intersection, end1) and is_between(start2, intersection, end2):
+        return intersection
+    return None
