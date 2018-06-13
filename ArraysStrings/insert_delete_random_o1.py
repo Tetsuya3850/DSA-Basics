44d2bc47ef403ae7123c6393709e462d7edfbf57
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-06-13 15:09:33 +0900

    get random

---
+++ ArraysStrings/insert_delete_random_o1.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import random
+
+
+class RandomizedSet:
+    def __init__(self):
+        self.elements = []
+        self.index_lookup = defaultdict()
+
+    def insert(self, val):
+        if val not in self.index_lookup:
+            self.elements.append(val)
+            self.index_lookup[val] = len(self.elements) - 1
+            return True
+        return False
+
+    def remove(self, val):
+        if val in self.index_lookup:
+            index = self.index_lookup[val]
+            self.elements[index], self.elements[-1] = self.elements[-1], self.elements[index]
+            self.index_lookup[self.elements[index]] = index
+            del self.index_lookup[val]
+            self.elements.pop()
+            return True
+        return False
+
+    def getRandom(self):
+        rand_i = random.randrange(len(self.elements))
+        return self.elements[rand_i]
